18576bee2adaed8382393929a0d2131c2823ee63
carlos warley <carloswarley0@gmail.com>
2026-08-11 17:44:11 -0300

    update struture past

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,13 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { routes } from "./index.js";
 
 export const app = Fastify({
-    logger: true,
+  logger: true,
+});
+
+await app.register(cors, {
+  origin: "http://127.0.0.1:5500",
 });
 
 app.register(routes);
